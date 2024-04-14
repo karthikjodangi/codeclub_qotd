e4e300c28a8de7771f6ad4fb51da2c32e8a9cd6c
Karthik <karthikjodangi333@gmail.com>
2024-04-14 13:36:19 +0530

    week-1

---
+++ day7.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int start,end;
+        for(int i=0;i<n;i++){
+            if(s[i] == 'B'){
+                start = i;
+                break;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='B'){
+                end = i;
+                break;
+            }
+        }
+        cout << end-start+1 << endl;
+    }
+}
